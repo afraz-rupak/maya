feab2103644c75243ac322872761505143e19cdd
Afraz Ul Haque <afrazrupak@Afrazs-MacBook-Pro.local>
2026-01-22 01:08:46 +1100

    Fix cryptography import - remove unused PBKDF2 import

--- frontend/components/secure_storage.py
+++ frontend/components/secure_storage.py
@@ -9,8 +9,6 @@ import pickle
 import hashlib
 from pathlib import Path
 from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2
 
 
 class SecureStorage:
